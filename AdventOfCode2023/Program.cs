67165734e7e19a67e447c981f57aa7b5017d1951
Jucci16 <John.Ucci3@gmail.com>
2023-12-15 16:21:56 -0500

    Day 2

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2023;
 
-Day1.Q1(Day1.Prompt());
+// Day1.Q1(Day1.Prompt());
+Console.WriteLine(Day2.Q2(Day2.Input()));
